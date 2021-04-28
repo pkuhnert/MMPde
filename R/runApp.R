e3ff2e3696fb0c50c0107a2bffd37cc603b6b7e1
Petra Kuhnert <Petra.Kuhnert@data61.csiro.au>
2021-04-28 12:45:25 +0930

    compressed data and fixed help file

--- R/runApp.R
+++ R/runApp.R
@@ -1,13 +1,8 @@
 #' Run shiny app
-#'
+#' 
+#' Function to run the shiny app
 #'
 #'@export
 runShiny <- function() {
-  library(shiny)
-  
-  #source(system.file("shinyApp", "server.r", package = "mmpdesigne"))
-  #source(system.file("shinyApp", "ui.r", package = "mmpdesigne"))
-  
-  #app <- shinyApp(ui, server)
   runApp(system.file("shinyApp", package="MMPde"))
 }
